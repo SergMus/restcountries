d60853101ba052fa528c715145c0b514d31f8fed
SergMus <umt1@ukr.net>
2023-02-22 13:14:23 +0300

    feat: add media-queries and modal hostlistener

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,7 +13,19 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
-  @Output() private isVisible = new EventEmitter<boolean>();
+  @Output() public isVisible = new EventEmitter<boolean>();
+  @ViewChild('modal') public modalWindow?: ElementRef;
+
+  @HostListener('click', ['$event'])
+  public clickOut(event: MouseEvent): void {
+    if (
+      !(this.modalWindow?.nativeElement as HTMLElement).contains(
+        event.target as Element
+      )
+    ) {
+      this.close();
+    }
+  }
 
   public close(): void {
     this.isVisible.emit(false);
